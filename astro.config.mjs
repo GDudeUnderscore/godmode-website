a327565b3f88f5dc058d731a125743ea18295f38
Dylan <djlevy26@gmail.com>
2024-12-20 22:06:04 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "astro/config";
+import fulldev from "fulldev-ui/integration";
+
+export default defineConfig({
+    integrations: [fulldev({
+        css: "/src/css/custom.css",
+        colors: {
+            theme: "dark",
+            dark: {
+                background: "#111110",
+                base: "#6F6D66",
+                brand: "#F50",
+            },
+            light: {
+                background: "#EEEEEC",
+                base: "#6F6D66",
+                brand: "#F50",
+            },
+        },
+    })]
+});
